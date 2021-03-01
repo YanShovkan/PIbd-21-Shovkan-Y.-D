36e047d924c52b4ac1c74743e3ad0bf5510c08ae
shovk <shovk@DESKTOP-NSNKSRN>
2021-03-01 17:06:55 +0400

    fix

---
+++ GiftShop/GiftShopDatabaseImplement/GiftShopDatabase.cs
@@ -0,0 +1,22 @@
+﻿using GiftShopDatabaseImplement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GiftShopDatabaseImplement
+{
+    public class GiftShopDatabase : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=HOME\SQLEXPRESS;Initial
+Catalog=AbstractShopDatabase;Integrated Security=True;MultipleActiveResultSets=True;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+        public virtual DbSet<Material> Materials { set; get; }
+        public virtual DbSet<Gift> Gifts { set; get; }
+        public virtual DbSet<GiftsMaterial> GiftsMaterials { set; get; }
+        public virtual DbSet<Order> Orders { set; get; }
+    }
+}
