a4d9c8a0bab2698df7c553264b50b0d3a117355c
shovk <shovk@DESKTOP-NSNKSRN>
2021-03-22 11:54:51 +0400

    try

--- GiftShop/GiftShopBusinessLogic/ViewModels/ReportGiftMaterialViewModel.cs
+++ GiftShop/GiftShopBusinessLogic/ViewModels/ReportGiftMaterialViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace GiftShopBusinessLogic.ViewModels
 {
     public class ReportProductComponentViewModel
     {
+        [DisplayName("Имя метериала")]
         public string MaterialName { get; set; }
+        [DisplayName("Итоговое количество")]
         public int TotalCount { get; set; }
         public List<Tuple<string, int>> Gifts { get; set; }
     }
